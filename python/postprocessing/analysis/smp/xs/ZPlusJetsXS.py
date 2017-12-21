78c2f74635ce4110f8124a0f0ba25a011dbc1feb
Salvatore Rappoccio <rappoccio@gmail.com>
2017-12-20 19:44:46 -0500

    Still broken

--- python/postprocessing/analysis/smp/xs/ZPlusJetsXS.py
+++ python/postprocessing/analysis/smp/xs/ZPlusJetsXS.py
@@ -14,9 +14,9 @@ class ZPlusJetsXS(Module):
         self.verbose = False
     def beginJob(self, histFile, histDirName):
         Module.beginJob(self, histFile, histDirName)
-        self.mBin = array.array('d', [0, 5, 10, 20, 40, 60, 80, 100]) #array.array('d', [0, 5, 10, 20, 40, 60, 80, 100 ,150, 200, 350])
+        self.mBin = array.array('d', [0, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]) #array.array('d', [0, 5, 10, 20, 40, 60, 80, 100 ,150, 200, 350])
         self.nbinsm = len(self.mBin) - 1
-        self.mBinB = array.array('d', [0, 2.5, 5, 7.5, 10, 15, 20,30, 40,50, 60, 70, 80, 90,100 ,125,150,175, 200,225, 250,275, 300, 325,350])
+        self.mBinB = array.array('d', [0, 0.5, 1, 3, 5, 7.5, 10, 15, 20,30, 40,50, 60, 70, 80, 90,100 ,125,150,175, 200,225, 250,275, 300, 325,350])
         self.nbinsmB = len(self.mBinB) - 1
 
         self.minDPhiZJet = 2.0
@@ -28,6 +28,9 @@ class ZPlusJetsXS(Module):
         self.addObject( ROOT.TH1F('h_gen',    'h_gen',    self.nbinsm, self.mBin) )
         self.addObject( ROOT.TH1F('h_fake',   'h_fake',   self.nbinsm, self.mBin) )
         self.addObject( ROOT.TH1F('h_miss',   'h_miss',   self.nbinsm, self.mBin) )
+        self.addObject( ROOT.TH1F('h_drGenReco',    'h_drGenReco',    40, 0, 0.8) )
+        self.addObject( ROOT.TH1F('h_drGenGroomed', 'h_drGenGroomed', 40, 0, 0.8) )
+                            
     def endJob(self):
         Module.endJob(self)
         pass
@@ -83,7 +86,7 @@ class ZPlusJetsXS(Module):
                 print '-----'
                 print 'all genjets:'
                 self.printCollection( allgenjets )
-            genjets = [ x for x in allgenjets if x.p4().DeltaPhi( Zboson.p4() ) > self.minDPhiZJet ]
+            genjets = [ x for x in allgenjets if x.p4().Perp() > 150. and x.p4().DeltaPhi( Zboson.p4() ) > self.minDPhiZJet ]
             # List of gen subjets (no direct link from Genjet):
             gensubjets = list(Collection(event, "SubGenJetAK8"))
             # Dictionary to hold ungroomed-->groomed for gen
@@ -92,7 +95,7 @@ class ZPlusJetsXS(Module):
             for igen,gen in enumerate(genjets):
                 gensubjetsMatched = self.getSubjets( p4=gen.p4(),subjets=gensubjets, dRmax=0.8)
                 genjetsGroomed[gen] = sum( gensubjetsMatched, ROOT.TLorentzVector() ) if len(gensubjetsMatched) > 0 else None
-
+                self.h_drGenGroomed.Fill( gen.p4().DeltaR( genjetsGroomed[gen] ) )
             if self.verbose:
                 print '----'
                 print 'opposite-Z genjets:'
@@ -123,13 +126,13 @@ class ZPlusJetsXS(Module):
             print '----'
             print 'all recojets:'
             self.printCollection( allrecojets )
-        recojets = [ x for x in allrecojets if x.p4().DeltaPhi( Zcand ) > self.minDPhiZJet ]
+        recojets = [ x for x in allrecojets if x.p4().Perp() > 200. and x.p4().DeltaPhi( Zcand ) > self.minDPhiZJet ]
         if isMC == False:
             genjets = [None] * len(recojets)
         # List of reco subjets:
         recosubjets = list(Collection(event,"SubJet"))
         # Dictionary to hold reco--> gen matching
-        recoToGen = matchObjectCollection( recojets, genjets, dRmax=0.1 )
+        recoToGen = matchObjectCollection( recojets, genjets, dRmax=0.05 )
         # Dictionary to hold ungroomed-->groomed for reco
         recojetsGroomed = {}        
         # Get the groomed reco jets
@@ -158,7 +161,7 @@ class ZPlusJetsXS(Module):
             recoSD = recojetsGroomed[reco]
             if reco == None or recoSD == None :
                 continue
-            self.h_reco.Fill( recoSD.M() )
+            #self.h_reco.Fill( recoSD.M() )
             genval = None
             if gen != None:
                 genSD = genjetsGroomed[gen]
@@ -170,10 +173,12 @@ class ZPlusJetsXS(Module):
                             )
                     genval = genSD.M()
                     self.h_response.Fill( genSD.M(), recoSD.M() )
-                    self.h_gen.Fill( genSD.M() )
+                    self.h_gen.Fill( genSD.M() )                    
+                    self.h_reco.Fill( recoSD.M() )
+                    self.h_drGenReco.Fill( reco.p4().DeltaR(gen.p4()) )
 
-
-                    if recoSD.M() > 10 and genSD.M() > 10 and (genSD.M() / recoSD.M() < 0.5 or genSD.M() / recoSD.M() > 2.0) :
+                    #if recoSD.M() > 10 and genSD.M() > 10 and (genSD.M() / recoSD.M() < 0.5 or genSD.M() / recoSD.M() > 2.0) :
+                    if self.verbose:
                         print '---------------------------------'
                         print ' reco: %s %8.4f , gen : %s %8.4f ' % (
                             self.printP4(reco), recoSD.M(),
@@ -202,8 +207,8 @@ class ZPlusJetsXS(Module):
                 genSD = genjetsGroomed[gen]
                 if genSD == None :
                     continue
-                self.h_response.Fill( genSD.M(), -1.0 )
-                self.h_gen.Fill( genSD.M() )
+                #self.h_response.Fill( genSD.M(), -1.0 )
+                #self.h_gen.Fill( genSD.M() )
                 self.h_miss.Fill( genSD.M() )
 
         return True
--- python/postprocessing/analysis/smp/xs/unfold_test.py
+++ python/postprocessing/analysis/smp/xs/unfold_test.py
@@ -8,22 +8,41 @@ h_gen = f.Get('zjets/h_gen')
 h_fake = f.Get('zjets/h_fake')
 h_miss = f.Get('zjets/h_miss')
 
-h_reco.Add( h_fake, -1.0 )
+
+for i in xrange(h_response.GetNbinsX()):
+	sumj = 0.
+	for j in xrange(h_response.GetNbinsY()):
+		sumj += h_response.GetBinContent(i,j)	
+	if sumj > 0.0:
+		for j in xrange(h_response.GetNbinsY()):
+			h_response.SetBinContent( i, j, h_response.GetBinContent(i,j) / sumj )
+			h_response.SetBinError( i, j,  h_response.GetBinError(i,j) / sumj )
+
+
+#h_reco.Add( h_fake, -1.0 )
 #h_gen.Add( h_miss, -1.0 )
 
 tunfolder = ROOT.TUnfoldDensity(h_response,ROOT.TUnfold.kHistMapOutputHoriz,ROOT.TUnfold.kRegModeNone, ROOT.TUnfold.kEConstraintNone, ROOT.TUnfoldDensity.kDensityModeBinWidth) 
 tunfolder.SetInput( h_reco )
+
+#scanResult = ROOT.TSpline3()
+#tunfolder.ScanTau(1000,0.00001,0.00005,scanResult,ROOT.TUnfoldDensity.kEScanTauRhoAvg)
+
+
 tunfolder.DoUnfold(0.0)
 h_unfolded = tunfolder.GetOutput("unfolded")
 
-
-h_gen.SetLineColor(ROOT.kBlue)
-h_unfolded.SetLineColor(ROOT.kRed)
+h_reco.SetLineColor(ROOT.kBlack)
+h_gen.SetLineColor(ROOT.kRed)
+h_unfolded.SetMarkerStyle(20)
+h_unfolded.SetMarkerColor(ROOT.kRed)
 
 c1 = ROOT.TCanvas("c1", "c1")
-h_gen.Draw()
-h_reco.Draw('same')
-h_unfolded.Draw('same')
+hs = ROOT.THStack("hs", "hs")
+hs.Add( h_gen, "hist" )
+hs.Add( h_reco, "hist" )
+hs.Add( h_unfolded, "e" )
+hs.Draw("nostack")
 
 c2 = ROOT.TCanvas("c2", "c2")
 h_response.Draw("colz")
