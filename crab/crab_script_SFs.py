05808ecb0609b88b8f42f96a5ca06e69df3c437e
UBParker <ash.marie.parker@gmail.com>
2018-01-24 15:13:21 -0600

    manual JSON for now, NOTE: this is a hack and only works for data

--- crab/crab_script_SFs.py
+++ crab/crab_script_SFs.py
@@ -13,7 +13,7 @@ from  PhysicsTools.NanoAODTools.postprocessing.analysis.jmar.sf.TTbar_SemiLep im
 
 #'Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
 #inputFiles()
-p=PostProcessor(".", inputFiles()  ,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&& ( (nElectron > 0 && HLT_Ele115_CaloIdVT_GsfTrkIdT) || (nMuon > 0 && HLT_Mu50))" ,modules=[TTbar_SemiLep()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis() ,histFileName= 'TTbar_SemiLep_hists.root', histDirName='ttbar_semilep'  , postfix='_ttbar_semilep-Skim', haddFileName=  'TTbar_SemiLep_hadded.root'  )
+p=PostProcessor(".", inputFiles()  ,"nFatJet>0&&FatJet_msoftdrop>30&&FatJet_pt>200&&MET_sumEt>40&& ( (nElectron > 0 && HLT_Ele115_CaloIdVT_GsfTrkIdT) || (nMuon > 0 && HLT_Mu50))" ,modules=[TTbar_SemiLep()],provenance=True,fwkJobReport=True,jsonInput='Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt' ,histFileName= 'TTbar_SemiLep_hists.root', histDirName='ttbar_semilep'  , postfix='_ttbar_semilep-Skim', haddFileName=  'TTbar_SemiLep_hadded.root'  )
 
 p.run()
 
